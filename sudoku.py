499dd276af47c5376fa796257b72899a3412e09b
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2018-12-19 20:18:48 +0530

    minor changes

--- sudoku.py
+++ sudoku.py
@@ -14,19 +14,17 @@ def main(image_path):
     print(grid)
     solution = sudoku_solver(grid)
     solution = solution.astype(int) 
+    print('Solution')
     print(solution)    
     
     
     
 if __name__ == '__main__':
-    image_path = 'images/sudoku2.jpg'
-    main(image_path)
-    
-# =============================================================================
-#     try:
-#         main(image_path = sys.argv[1])
-#     except:             #    except IndexError:
-#         fmt = 'usage: {} image_path'
-#         print(fmt.format(__file__.split('/')[-1]))
-#         print('[ERROR]: Image not found')
-# =============================================================================
+#    image_path = 'images/sudoku2.jpg'
+#    main(image_path)
+    try:
+        main(image_path = sys.argv[1])
+    except:             #    except IndexError:
+        fmt = 'usage: {} image_path'
+        print(fmt.format(__file__.split('/')[-1]))
+        print('[ERROR]: Image not found')
